47e2aa3bf67a6d129bf952c50d4e5d519ed84d79
jianghang <414359193@qq.com>
2023-03-13 23:37:35 +0800

    feat:  添加个人中心页面

--- src/view/profile/index.vue
+++ src/view/profile/index.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-   个人中心
+    <ElCard>
+      <div>
+        <h2>账号管理</h2>
+      </div>
+      <ElTabs v-model="activeName">
+        <ElTabPane label="User" name="first">基本资料</ElTabPane>
+        <ElTabPane label="Config" name="second">修改头像</ElTabPane>
+        <ElTabPane label="Role" name="third">修改密码</ElTabPane>
+      </ElTabs>
+    </ElCard>
   </div>
 </template>
 
-<script  lang="ts">
+<script lang="ts">
 export default defineComponent({
   title: '个人中心',
-  hideMenu:true
+  hideMenu: true,
 })
-
 </script>
 <script setup lang="ts">
-
+const activeName = ref('first')
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
