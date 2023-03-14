a30ded8a6a95c5353c17507f9db3a33f616ef8cf
jianghang <414359193@qq.com>
2023-03-14 22:54:18 +0800

    feat: 添加邮箱修改弹框

--- src/baseView/userPermission/login/index.vue
+++ src/baseView/userPermission/login/index.vue
@@ -2,28 +2,40 @@
   <div class="login-container">
     <div class="login-content">
       <Transition appear name="login">
-        <div class="login-icon-img" style="--offset-x:-100px">
+        <div class="login-icon-img" style="--offset-x: -100px">
           <img src="@/assets/mange-icon.png" />
           <p class="login-title">智能点名后端管理系统</p>
         </div>
       </Transition>
-      <Transition appear name="login" style="--offset-x:100px">
+      <Transition appear name="login" style="--offset-x: 100px">
         <div class="login-form">
           <h2 class="login-form__title">登录</h2>
-          <ElForm @submit.prevent :model="loginForm" :rules="rules" ref="ruleFormRef">
+          <ElForm
+            @submit.prevent
+            :model="loginForm"
+            :rules="rules"
+            ref="ruleFormRef"
+          >
             <ElSpace :size="10" fill class="fill">
               <ElFormItem prop="email">
                 <ElInput v-model="loginForm.email" placeholder="请输入邮箱" />
               </ElFormItem>
               <ElFormItem prop="password">
-                <ElInput type="password" placeholder="请输入密码" v-model="loginForm.password" />
+                <ElInput
+                  type="password"
+                  placeholder="请输入密码"
+                  v-model="loginForm.password"
+                />
               </ElFormItem>
             </ElSpace>
             <ElSpace :size="10" fill class="fill">
               <ElRow justify="space-between">
                 <ElCol :span="5">
                   <ElFormItem prop="remember">
-                    <ElCheckbox label="记住我" v-model="loginForm.remember"></ElCheckbox>
+                    <ElCheckbox
+                      label="记住我"
+                      v-model="loginForm.remember"
+                    ></ElCheckbox>
                   </ElFormItem>
                 </ElCol>
                 <ElCol :span="5.5">
@@ -31,7 +43,14 @@
                 </ElCol>
               </ElRow>
               <ElFormItem>
-                <ElButton type="primary" :loading="loading" :size="'large'" class="fill" @click="submit">登录</ElButton>
+                <ElButton
+                  type="primary"
+                  :loading="loading"
+                  :size="'large'"
+                  class="fill"
+                  @click="submit"
+                  >登录</ElButton
+                >
               </ElFormItem>
             </ElSpace>
           </ElForm>
@@ -44,12 +63,12 @@
 <script setup lang="ts">
 import { FormInstance, FormRules } from 'element-plus'
 import { omit } from '@/utils/object'
-import userStore from '@/store/userStore';
+import userStore from '@/store/userStore'
 const loading = ref(false)
 type FormType = {
-  email: string,
-  password: string,
-  remember: boolean,
+  email: string
+  password: string
+  remember: boolean
 }
 const ruleFormRef = ref<FormInstance>()
 const loginForm = reactive<FormType>({
@@ -59,12 +78,13 @@ const loginForm = reactive<FormType>({
 })
 
 const rules = reactive<FormRules>({
-  email: [{ required: true, message: '请输入邮箱', trigger: 'blur' },
-  {
-    type: 'email',
-    message: '请输入合法邮箱',
-    trigger: ['blur'],
-  }
+  email: [
+    { required: true, message: '请输入邮箱', trigger: 'blur' },
+    {
+      type: 'email',
+      message: '请输入合法邮箱',
+      trigger: ['blur'],
+    },
   ],
   password: [
     {
@@ -78,12 +98,11 @@ const rules = reactive<FormRules>({
 const user = userStore()
 const submit = async () => {
   if (!ruleFormRef) return
-  const v = await ruleFormRef.value?.validate()
+  const v = await ruleFormRef.value?.validate().catch(() => {})
   if (!v) return
   loading.value = true
   try {
     await user.login(omit(loginForm, ['remember']))
-
   } finally {
     loading.value = false
   }
--- src/view/profile/children/account.vue
+++ src/view/profile/children/account.vue
@@ -21,10 +21,18 @@
     </div>
     <ElForm>
       <ElFormItem label="姓名：">
-        <ElInput model-value="蔡夏柠" placeholder="请输入姓名" readonly />
+        <ElInput
+          :model-value="user.userInfo.userName"
+          placeholder="请输入姓名"
+          readonly
+        />
       </ElFormItem>
       <ElFormItem label="学号：">
-        <ElInput model-value="212106601" placeholder="请输入学号" readonly />
+        <ElInput
+          :model-value="user.userInfo.account"
+          placeholder="请输入学号"
+          readonly
+        />
       </ElFormItem>
       <ElFormItem label="性别：">
         <ElRadioGroup
@@ -38,24 +46,83 @@
       </ElFormItem>
       <ElFormItem label="班级：">
         <ElInput
-          model-value="计算机科学与技术"
+          :model-value="user.userInfo.class?.className"
           placeholder="请选择班级"
           readonly
         />
       </ElFormItem>
       <ElFormItem label="系别：">
-        <ElInput model-value="信息工程系" placeholder="请选择班级" readonly />
+        <ElInput
+          :model-value="user.userInfo.class?.departmentId ?? '未知'"
+          placeholder="请选择系别"
+          readonly
+        />
       </ElFormItem>
       <ElFormItem label="邮箱：">
         <div class="flex items-center">
           <ElInput
-            model-value="信息工程系"
+            :model-value="user.userInfo.email"
             placeholder="请输入邮箱"
             readonly
-          /><ElIcon :size="20" class="cursor-pointer"> <Edit /></ElIcon>
+          /><ElIcon :size="20" class="cursor-pointer" @click="emailShow = true">
+            <Edit
+          /></ElIcon>
         </div>
       </ElFormItem>
     </ElForm>
+
+    <ElDialog
+      destroy-on-close
+      v-model="emailShow"
+      title="修改邮箱"
+      :show-close="false"
+      align-center
+      class="max-w-2xl"
+      @close="countForm = {}"
+    >
+      <ElForm
+        class="w-4/6 mx-auto"
+        :model="countForm"
+        :rules="rules"
+        ref="ruleFormRef"
+      >
+        <ElFormItem prop="email">
+          <ElInput
+            v-model="countForm.email"
+            :size="'large'"
+            placeholder="请输入邮箱"
+          >
+            <template #prefix>
+              <el-icon size="25"><Message /></el-icon>
+            </template>
+          </ElInput>
+        </ElFormItem>
+        <ElFormItem prop="captcha">
+          <div class="flex item-center w-full">
+            <ElInput
+              :size="'large'"
+              v-model="countForm.captcha"
+              placeholder="请输入验证码"
+            >
+              <template #prefix>
+                <svg class="icon" aria-hidden="true">
+                  <use xlink:href="#icon-yanzhengma"></use>
+                </svg>
+              </template>
+            </ElInput>
+            <ElButton class="ml-4" type="primary" round>发送验证码</ElButton>
+          </div>
+        </ElFormItem>
+      </ElForm>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="emailShow = false">取消</el-button>
+          <el-button type="primary" :loading="loading" @click="submit">
+            确认
+          </el-button>
+        </span>
+      </template>
+    </ElDialog>
   </div>
 </template>
 
@@ -68,10 +135,33 @@ export default defineComponent({
 <script setup lang="ts">
 import { updateUser } from '@/http/api'
 import userStore from '@/store/userStore'
-import { Edit } from '@element-plus/icons-vue'
-import { ElMessage, UploadProps, UploadRawFile } from 'element-plus'
-const user = userStore()
+import { Edit, Message } from '@element-plus/icons-vue'
+import { ElMessage, FormInstance, FormRules, UploadProps } from 'element-plus'
 
+const ruleFormRef = ref<FormInstance>()
+const countForm = ref<{ email?: string; captcha?: string }>({})
+
+const rules = reactive<FormRules>({
+  email: [
+    { required: true, message: '请输入邮箱', trigger: 'blur' },
+    {
+      type: 'email',
+      message: '请输入合法邮箱',
+      trigger: ['blur'],
+    },
+  ],
+  captcha: [
+    {
+      required: true,
+      message: '请输入验证码',
+      trigger: 'blur',
+    },
+  ],
+})
+
+const loading = ref(false)
+const user = userStore()
+const emailShow = ref(false)
 // 更新性别
 const changeSex = async (e: any) => {
   await updateUser({ sex: e })
@@ -92,10 +182,22 @@ const beforeAvatarUpload: UploadProps['beforeUpload'] = rawFile => {
   }
   return true
 }
+
+const submit = async () => {
+  const data = await ruleFormRef.value?.validate().catch(() => {})
+  if (!data) return
+  loading.value = true
+  setTimeout(() => {
+    loading.value = false
+    emailShow.value = false
+  }, 2000)
+}
 </script>
 
 <style scoped lang="scss">
 .count-box {
   padding-left: 2rem;
+
+  @include useIcon(1.5rem);
 }
 </style>
