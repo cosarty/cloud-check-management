5801aaff0fcf5dc3651e38862a984cba757aa7dc
jianghang <414359193@qq.com>
2023-03-20 23:24:53 +0800

    feat: 添加学生界面

---
+++ src/view/student/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>学生管理</div>
+</template>
+<script lang="ts">
+export default defineComponent({
+  title: '学生',
+  icon: 'icon-bumen-department',
+  name: 'student',
+  auth: 'super',
+})
+</script>
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/view/teacher/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>班级管理</div>
+</template>
+<script lang="ts">
+export default defineComponent({
+  title: '班级',
+  icon: 'icon-bumen-department',
+  name: 'teacher',
+  auth: 'super',
+})
+</script>
+<script setup lang="ts"></script>
+
+<style scoped></style>
