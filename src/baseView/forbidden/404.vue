28f60158b7f65396625f0ecc7495fd8568d77abb
cosarty <1398675906@qq.com>
2023-05-04 10:41:25 +0800

    feat: 修复404bug

--- src/baseView/forbidden/404.vue
+++ src/baseView/forbidden/404.vue
@@ -1,28 +1,34 @@
-<script setup lang="ts">
-
-</script>
 
 <template>
   <div class="bg-gradient-to-r from-purple-300 to-blue-200  min-h-full">
     <div class="w-full py-16 min-h-full flex items-center justify-center">
       <div class="bg-white shadow overflow-hidden sm:rounded-lg pb-8 px-28">
-      <div class="border-t border-gray-200 text-center pt-8">
-        <h1 class="text-9xl font-bold text-purple-400">404</h1>
+        <div class="border-t border-gray-200 text-center pt-8">
+          <h1 class="text-9xl font-bold text-purple-400">404</h1>
           <h1 class="text-3xl font-medium py-8">你访问的页面不存在</h1>
           <!-- <p
   						class="text-2xl pb-8 px-12 font-medium"
   					>Oops! The page you are looking for does not exist. It might have been moved or deleted.</p>-->
-          <router-link to="/"
-            class="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">首页</router-link>
+          <button @click="back"
+            class="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">首页</button>
         </div>
       </div>
     </div>
   </div>
 </template>
 
+<script setup lang="ts" >
+import { CacheEnum } from '@/enum/CacheEnum'
+import storage from '@/utils/storage'
+const router = useRouter()
+const  back = ()=>{
+  storage.remove(CacheEnum.HISTORY_MENU)
+  router.push('/')
+
+}
+</script>
+
 <style lang="scss" scoped>
-
-
 .text-9xl {
   font-size: 14rem;
 }
@@ -59,4 +65,5 @@
     padding-left: 1rem;
     padding-right: 1rem;
   }
-}</style>
+}
+</style>
